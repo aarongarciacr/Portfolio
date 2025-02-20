cfb7514ee26c7654376a884d1c681a7e61cf6e78
aarongarciacr <aarongarcia4997@gmail.com>
2025-02-19 18:28:54 -0700

    Testing Responsiveness on tailwind

--- src/components/LandingPage/Greeting.tsx
+++ src/components/LandingPage/Greeting.tsx
@@ -9,27 +9,31 @@ const Greeting = () => {
   };
 
   return (
-    <div className="h-[140svh] bg-slate-200">
-      <div className="m-auto flex h-[75%] w-[70%] items-start justify-around">
-        <div className="relative m-auto flex flex-col items-start justify-end">
+    <div className="h-[140vh] bg-slate-200">
+      <div className="m-auto flex h-[75%] w-[95%] flex-col items-center justify-center gap-8 md:w-[70%] md:flex-row md:items-start md:justify-around">
+        <div className="relative m-auto flex flex-col items-center text-center md:items-start md:text-left">
           <Reveal>
-            <p className="text-5xl font-extralight text-black">Hello, I'm</p>
+            <p className="text-3xl font-extralight text-black sm:text-blue-500 md:text-5xl">
+              Hello, I'm
+            </p>
           </Reveal>
           <Reveal>
-            <p className="pb-3 text-7xl font-bold text-black">Aaron Garcia</p>
+            <p className="pb-3 text-5xl font-bold text-black md:text-7xl">
+              Aaron Garcia
+            </p>
           </Reveal>
           <Reveal>
             <button
               onClick={scrollToContact}
-              className="rounded-sm border-gray-200 bg-gray-700 p-3 px-9 text-2xl text-gray-200 hover:border-transparent hover:bg-gray-500 hover:text-white active:bg-gray-600"
+              className="rounded-sm border-gray-200 bg-gray-700 p-2 px-6 text-xl text-gray-200 hover:border-transparent hover:bg-gray-500 hover:text-white active:bg-gray-600 md:p-3 md:px-9 md:text-2xl"
             >
               Get in Touch
             </button>
           </Reveal>
         </div>
-        <div className="w-[500px] overflow-hidden rounded-2xl">
+        <div className="w-[350px] overflow-hidden rounded-2xl md:w-[600px]">
           <Reveal>
-            <img src={ProfileImg}></img>
+            <img src={ProfileImg} alt="Profile" />
           </Reveal>
         </div>
       </div>
